4a12105193492d841248013a0cb6e3efe636c784
Mohan <mohandeblaze@gmail.com>
2024-01-15 14:04:41 +0530

    update build script

--- src/compose.ps1
+++ src/compose.ps1
@@ -7,67 +7,61 @@ $env:TIMESTAMP = $TIMESTAMP
 
 # print current directory
 Write-Host "Current directory: $pwd"
+Write-Host "Timestamp: $TIMESTAMP"
 
-function Build()
-{
-    docker compose build
+function Build() {
+  docker build . -t "mohandeblaze/project-planner:latest" -t "mohandeblaze/project-planner:${TIMESTAMP}"
 }
 
-function Push()
-{
-    docker push "mohandeblaze/project-planner:latest"
-    docker push "mohandeblaze/project-planner:$TIMESTAMP"
+function Push() {
+  docker push "mohandeblaze/project-planner:latest"
+  docker push "mohandeblaze/project-planner:$TIMESTAMP"
 }
 
-function RemoveLocalImages()
-{
-    docker image rm "mohandeblaze/project-planner:latest" -f
-    docker image rm "mohandeblaze/project-planner:$TIMESTAMP" -f
+function RemoveLocalImages() {
+  docker image rm "mohandeblaze/project-planner:latest" -f
+  docker image rm "mohandeblaze/project-planner:$TIMESTAMP" -f
 }
 
-function Run()
-{
-    docker compose up -d
-    docker-compose ps
+function Run() {
+  docker compose up -d
+  docker-compose ps
 }
 
 # Check the input parameter
-if ($args[0] -eq "publish")
-{
-    # build the image
-    Build
+if ($args[0] -eq "publish") {
+  # build the image
+  Build
+  $exitCode = $LASTEXITCODE
+
+  # Check if the build was successful
+  if ($exitCode -eq 0) {
+    Push
     $exitCode = $LASTEXITCODE
 
-    # Check if the build was successful
-    if ($exitCode -eq 0)
-    {
-        Push
-        $exitCode = $LASTEXITCODE
-
-        # Check if the push was successful
-        if ($exitCode -eq 0)
-        {
-            # Remove local images
-            RemoveLocalImages
-        }
+    # Check if the push was successful
+    if ($exitCode -eq 0) {
+      # Remove local images
+      RemoveLocalImages
     }
+  }
 }
-elseif ($args[0] -eq "local")
-{
-    # build the image
-    Build
-    $exitCode = $LASTEXITCODE
+elseif ($args[0] -eq "build") {
+  # build the image
+  Build
+}
+elseif ($args[0] -eq "local") {
+  # build the image
+  Build
+  $exitCode = $LASTEXITCODE
 
-    if ($exitCode -eq 0)
-    {
-        Run
-    }
-}
-elseif ($args[0] -eq "up")
-{
+  if ($exitCode -eq 0) {
     Run
+  }
 }
-else
-{
-    Write-Host "Usage: $args[0] [publish | local]"
+elseif ($args[0] -eq "up") {
+  Run
+}
+else {
+  Write-Host "Usage: $args[0] [publish | local]"
 }
