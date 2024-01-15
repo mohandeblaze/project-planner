46ac99e6bd140c0575a25910bc0d6d4b39c4d7e5
Mohan <mohandeblaze@gmail.com>
2024-01-15 11:56:58 +0530

    update build script

--- src/compose.ps1
+++ src/compose.ps1
@@ -5,22 +5,64 @@ $env:PG_DB_PROJECT_PLANNER_PASSWORD = $env:PG_DB_PROJECT_PLANNER_PASSWORD
 $TIMESTAMP = Get-Date -Format "yyyy.MM.dd.HH.mm.ss"
 $env:TIMESTAMP = $TIMESTAMP
 
-# current directory
-$pwd = (Get-Location).Path
+# print current directory
+Write-Host "Current directory: $pwd"
+
+function Build()
+{
+    docker compose build
+}
+
+function Push()
+{
+    docker push "mohandeblaze/project-planner:latest"
+    docker push "mohandeblaze/project-planner:$TIMESTAMP"
+}
+
+function RemoveLocalImages()
+{
+    docker image rm "mohandeblaze/project-planner:latest" -f
+    docker image rm "mohandeblaze/project-planner:$TIMESTAMP" -f
+}
+
+function Run()
+{
+    docker compose up -d
+}
 
 # Check the input parameter
-if ($args[0] -eq "build") {
-  # Run docker-compose build
-  docker compose build
+if ($args[0] -eq "publish")
+{
+    # build the image
+    Build
+    $exitCode = $LASTEXITCODE
 
-  # Push the built image to Docker Hub
-  docker push "mohandeblaze/project-planner:latest"
-  docker push "mohandeblaze/project-planner:$TIMESTAMP"
+    # Check if the build was successful
+    if ($exitCode -eq 0)
+    {
+        Push
+        $exitCode = $LASTEXITCODE
+
+        # Check if the push was successful
+        if ($exitCode -eq 0)
+        {
+            # Remove local images
+            RemoveLocalImages
+        }
+    }
 }
-elseif ($args[0] -eq "up") {
-  # Run docker-compose up
-  docker compose up -d
+elseif ($args[0] -eq "local")
+{
+    # build the image
+    Build
+    $exitCode = $LASTEXITCODE
+
+    if ($exitCode -eq 0)
+    {
+        Run
+    }
 }
-else {
-  Write-Host "Usage: $args[0] [build | up]"
+else
+{
+    Write-Host "Usage: $args[0] [publish | local]"
 }
