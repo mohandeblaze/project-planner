eb68e0376b26c0b0fa7a37c94874089800a26e38
Mohan <mohandeblaze@gmail.com>
2024-01-15 21:25:22 +0530

    [test] check if app can access postgres env variables

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,23 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/ping", () => new { status = "Alive", date = DateTime.UtcNow });
+app.MapGet("/ping", async () =>
+{
+    var postgresDb = Environment.GetEnvironmentVariable("POSTGRES_DB");
+    var postgresPasswordFile = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD_FILE");
+
+    Console.WriteLine($"POSTGRES_DB: {postgresDb}");
+    Console.WriteLine($"POSTGRES_PASSWORD_FILE: {postgresPasswordFile}");
+
+    if (postgresPasswordFile != null)
+    {
+        var password = await File.ReadAllTextAsync(postgresPasswordFile);
+
+        Console.WriteLine($"POSTGRES_PASSWORD length: {password.Length}");
+    }
+
+    return new { status = "Alive", date = DateTime.UtcNow };
+});
 
 Console.WriteLine("Application started");
 
