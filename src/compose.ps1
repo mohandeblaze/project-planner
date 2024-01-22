a86c1107524767a5c1ddf454b25e40452f215ea5
Mohan <mohandeblaze@gmail.com>
2024-01-22 11:58:55 +0530

    update build script

--- src/compose.ps1
+++ src/compose.ps1
@@ -47,6 +47,8 @@ function RemoveLocalImages() {
 }
 
 function DeployStack() {
+  docker pull "mohandeblaze/project-planner:latest"
+  docker pull "mohandeblaze/project-planner-worker:latest"
   docker stack deploy --compose-file docker-compose.yml project-planner-stack
 }
 
