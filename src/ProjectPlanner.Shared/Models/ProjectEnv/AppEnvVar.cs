e45918d5189093a8916e999b1be9a2286d7b5d23
Mohan <mohandeblaze@gmail.com>
2024-02-04 23:23:07 +0530

    fix env file read

--- src/ProjectPlanner.Shared/Models/ProjectEnv/AppEnvVar.cs
+++ src/ProjectPlanner.Shared/Models/ProjectEnv/AppEnvVar.cs
@@ -13,8 +13,8 @@ public partial class AppEnvVar
 
     public static async Task ReadAsync()
     {
-        var auth0Domain = await File.ReadAllTextAsync(ProjectEnv.Auth0DomainFile);
-        var auth0ClientId = await File.ReadAllTextAsync(ProjectEnv.Auth0ClientIdFile);
+        var auth0Domain = await File.ReadAllTextAsync(Environment.GetEnvironmentVariable(ProjectEnv.Auth0DomainFile)!);
+        var auth0ClientId = await File.ReadAllTextAsync(Environment.GetEnvironmentVariable(ProjectEnv.Auth0ClientIdFile)!);
 
         I = new AppEnvVar
         {
