937b7ad0c0ccb86402798f260b33f93c81a48c30
Mohan <mohandeblaze@gmail.com>
2024-07-06 21:52:24 +0530

    delete dumb next.js

--- server-old/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppType } from "next/app";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
-};
-
-export default MyApp;
--- server-old/src/pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { IHello } from "lib/hello";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<IHello>) {
-    res.status(200).json({ name: "John Doe" });
-}
