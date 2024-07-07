541c3c03fbf9c8f7939fd8a81ee7bd06186a1e35
Mohan <mohandeblaze@gmail.com>
2024-07-07 23:04:01 +0530

    update

--- build.ts
+++ build.ts
@@ -1,12 +1,12 @@
-import { build } from "esbuild";
-import { dependencies, peerDependencies } from "./package.json";
+import { build } from 'esbuild';
+import { dependencies, peerDependencies } from './package.json';
 
 build({
-    entryPoints: ["server/index.ts"],
+    entryPoints: ['server/index.ts'],
     bundle: true,
-    minify: true,
+    minify: false,
     external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
-    platform: "node",
-    format: "esm",
-    outfile: "dist/index.js",
+    platform: 'node',
+    format: 'esm',
+    outfile: 'dist/index.js',
 });
